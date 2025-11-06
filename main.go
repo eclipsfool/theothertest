3c80bcf5e25b071cf68e7ba4580dd0aef5ec78f9
eclipsfool <glebka76543@gmail.com>
2025-11-06 15:26:26 +0300

    Initial commit:pushed The Other

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type HomeData struct {
+	Title    string
+	VideoSrc string
+}
+
+var templates = template.Must(template.ParseFiles("templates/index.html"))
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.HandleFunc("/", handleHome)
+
+	log.Println("Server listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	data := HomeData{
+		Title:    "The Other",
+		VideoSrc: "/static/videos/trailer.mp4",
+	}
+	if err := templates.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
